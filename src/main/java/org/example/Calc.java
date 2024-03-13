7b1aa687cac5bfa04fc5bfc08f46df850192790c
Wytse Pechler <wytse.pechler@gmail.com>
2024-03-13 20:34:38 +0100

    First Commit

---
+++ src/main/java/org/example/Calc.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Calc {
+    public static int som(int a, int b) {
+        return a + b;
+    }
+
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+
+    public static double minus(double a, double b) {
+        return a - b;
+    }
+
+    public static double som(double a, double b) {
+        return a + b;
+    }
+}
