27fca9d7a9c9d3f3a786dac9457f75aff15ba238
Dequalix <deadly.xian@gmail.com>
2024-05-21 07:32:39 +0200

    TS

---
+++ z_java-klas-web/TSLabs/Lab1/HelloWorld.js
@@ -0,0 +1,8 @@
+"use strict";
+class HelloWorld {
+    static sayHello() {
+        console.log('Hello world');
+        // console.log(ddd);
+    }
+}
+HelloWorld.sayHello();
---
+++ z_java-klas-web/TSLabs/Lab1/HelloWorld.ts
@@ -0,0 +1,10 @@
+class HelloWorld {
+    static sayHello() {
+        console.log('Hello world');
+        // console.log(ddd);
+    }
+}
+
+HelloWorld.sayHello();
+
+var a: string = "hello";
